ee990f5a3943e3a9ae89fc6025ed78f96e41b990
Mateus Reis Bastos <241008513@aluno.unb.br>
2024-12-28 19:01:03 -0300

    Add new sequence generation programs: sequenceij1.cpp and sequenceij2.cpp

---
+++ c++/sequenceij1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    for (int i = 1, j = 60; i < 1000, j >= 0; i += 3, j -= 5)
+    {
+        cout << "I=" << i << " " << "J=" << j << endl;
+    }
+    
+    return 0;
+}
