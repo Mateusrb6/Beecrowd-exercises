b92bc74887ba45848ddfa109805d2d79a6cfbefb
Mateus Reis Bastos <241008513@aluno.unb.br>
2024-12-30 00:05:54 -0300

    initial commit

--- c++/sequenceij1.cpp
+++ c++/sequenceij1.cpp
@@ -5,7 +5,7 @@ int main(){
 
     for (int i = 1, j = 60; i < 1000, j >= 0; i += 3, j -= 5)
     {
-        cout << "I=" << i << " " << "J=" << j << endl;
+        cout << "I=" << i << " " << "J=" << j << "/n";
     }
     
     return 0;
